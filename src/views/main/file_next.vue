97be3d8e05d2fb7f3c42c76a82a25e6844312fa0
云岑.YCloud <xcenweb@qq.com>
2025-08-29 01:50:56 +0800

    update

--- src/views/main/file_next.vue
+++ src/views/main/file_next.vue
@@ -1,12 +1,12 @@
 <template>
-    <v-app>
+    <v-app style="margin-top: env(safe-area-inset-top);">
         <v-app-bar>
             <v-btn icon="mdi-arrow-left" @click="$router.back()" />
             <v-app-bar-title>
                 {{ $route.query.path?.toString().split('/').pop() || '' }}
             </v-app-bar-title>
         </v-app-bar>
-        <v-main style="margin-top: env(safe-area-inset-top);">
+        <v-main>
             <file-list :path="$route.query.path?.toString() || ''" />
         </v-main>
     </v-app>
